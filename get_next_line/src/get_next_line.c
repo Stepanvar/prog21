84e4c448d4b36b8a355f8d1ca1081859dc1b2c18
Clydas Curie <ccurie@su-e5.msk.21-school.ru>
2022-01-08 19:11:12 +0300

    save

--- get_next_line/src/get_next_line.c
+++ get_next_line/src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ccurie <ccurie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 12:15:58 by ccurie            #+#    #+#             */
-/*   Updated: 2022/01/07 16:18:00 by ccurie           ###   ########.fr       */
+/*   Updated: 2022/01/08 19:10:45 by ccurie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,21 @@
 
 char	*ft_onesplit(char *str, char c)
 {
+	int		i;
 	char	*buf;
 
+	i = 0;
+	i = (ft_strchr(str, c) - str + 1);
 	buf = ft_substr(str, 0, ft_strchr(str, c) - str + 1);
+	printf("\n|%d|", i);
+	printf("|buf %s|", buf);
+	buf[i + 1] = '\0';
 	return (buf);
 }
 
-//char	*ft_check(char *buf, char *str, int size)
-//{
-//	if (-1 == size)
-//	{
-//		free(str);
-//		free(buf);
-//		return (NULL);
-//	}
-//}
-
 char	*get_next_line(int fd)
 {
-	int		size;
+	int			size;
 	static char	*str = NULL;
 	char		*buf;
 
@@ -44,47 +40,42 @@ char	*get_next_line(int fd)
 	if (!str)
 	{
 		str = (char *)malloc(BUFFER_SIZE + 1);
-	
 		if (!str)
 			return (NULL);
 		size = read(fd, str, BUFFER_SIZE);
-		str[size + 1] = '\0';
 	}
 	buf = ft_onesplit(str, '\n');
-	if (-1 == size || 0 == size)
+	if (0 == size)
 	{
-		free(str);
-		if (0 == size)
-			return(buf);
-		else
-			return (NULL);
+		str = NULL;
+		return (buf);
 	}
-	str += ft_strlen(buf) - 1;
+	str += ft_strlen(buf);
 	return (buf);
 }
 
- int	main(int argc, char *argv[])
- {
- 	int		fd;
- 	int		i;
- 	char	*str;
+int	main(int argc, char *argv[])
+{
+	int		fd;
+	int		i;
+	char	*str;
 
- 	fd = 0;
- 	i = 0;
- 	if (argc == 1)
- 	{
- 		fd = open(argv[1], O_RDONLY);
- 		if (-1 == fd)
- 			return (-1);
- 	}
- 	str = get_next_line(fd);
+	fd = 0;
+	i = 0;
+	if (argc > 1)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (-1 == fd)
+			return (-1);
+	}
+	str = get_next_line(fd);
 	printf("%s", str);
- 	while (i < 10)
- 	{
- 		str = get_next_line(fd);
+	while (i < 4)
+	{
+		str = get_next_line(fd);
 		printf("%s", str);
- 		i++;
- 	}
- 	close(fd);
- 	return (0);
- }
+		i++;
+	}
+	close(fd);
+	return (0);
+}
